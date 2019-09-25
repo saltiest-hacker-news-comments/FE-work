4a606d555d0e9184109ead891fbc58d0ab277f91
Derek Etman <dereketman@gmail.com>
2019-09-24 20:40:04 -0700

    + account Card
    Co-authored-by: Akak Almazbekova <akak.almazbekova@gmail.com>

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -5,22 +5,25 @@ import { UsersInfo } from './LandingPage/TopUsers';
 
 const AccountList = (props) => {
   const [accounts, setAccounts] = useState([UsersInfo]);
-    
-//   return (
-//   <div className="divOutsideMap">Test1
-//    <div className="divInsideMap">Test2</div>   
-//     </div>
-// );
-
   return (
-    <div className="divOutsideMap">
-        {accounts.map(data => {
-            return (
-               <div className="divInsideMap"></div>
-            );
-        })}
+    <div>
+      <div>
+        <h1>Welcome to your account</h1>
+      </div>
+      <div className="divOutsideMap">
+        <h3>Your Saved Comments</h3>
+        <div className="divInsideMap">
+          <AccountCard />
+        </div>
+        {/* {accounts.map(data => {
+              return (
+                <div className="divInsideMap">   
+                </div>
+              );
+          })} */}
+      </div>
     </div>
-)
+  )
 }
 
 export default AccountList;
