c2c63298ff8f027605adfb127daac36578a6a809
Derek Etman <dereketman@gmail.com>
2019-09-25 22:09:15 -0700

    +Displaying Fav +Saving Fav +Delete Fav
    Co-authored-by: Akak Almazbekova <akak.almazbekova@gmail.com>
    Co-authored-by: Brandy Becker <BrandyBeckerFSW@gmail.com>
    Co-authored-by: Dakotah Huey <iggylow1@gmail.com>

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -5,26 +5,35 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 import { UserContext } from '../context/UserContext';
 
 
-const AccountList = (props) => {
+const AccountList =  ( props ) => {
   const { user } = useContext(UserContext);
-
   const [favorite, setFavorite] = useState([]);
+  // console.log("FAVORITE: ", favorite)
+  // console.log("USER: ", user.token) // returns empty array currently?
+
+  useEffect(() => {
+        axiosWithAuth()
+          .get(`/comments/allfavsalt`) // {user} should probably be user.id or something
+          .then(response => {
+            // console.log('RESPONSE: ', response)
+            setFavorite(response.data) // set to data from response, currently empty array? maybe user.id when it's not empty
+          })
+          .catch(error => {
+            console.log('ERROR: ', error)
+          })
+    }, [])
+
 
   return (
     <div>
-        <h1>{user}</h1>
-      <div className="divOutsideMap">
-          <h3>Your Saved Comments</h3>
-          <div className="divInsideMap">
-            <AccountCard />
-          </div>
-          {/* {accounts.map(data => {
-                return (
-                  <div className="divInsideMap">   
-                  </div>
-                );
-            })} */}
-      </div>
+      <h1>{user.message}</h1>
+
+        <div className="divOutsideMap">
+            <h3>Your Saved Comments</h3>
+            <div className="divInsideMap">
+               {favorite.map(data =>  <AccountCard data={data} /> )}
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,37 +1,60 @@
-import React from 'react'
-import { Comment } from 'semantic-ui-react'
+import React, {useEffect} from 'react';
+import { Comment } from 'semantic-ui-react';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 
 
 
-const CommentCard = (props) => (
+const CommentCard = (props) => {
+
+  // De-structured Array
+  const {id} = props.data;
+  // You can use these de-structured arrays to call props 
+  // example: you can use author instead of props.author
+  const {author, data, comment, score, key, portrait} = props;
+  console.log(key); // currently undefined 
+
+  // salt shaker profile picture for user comments vvvv
+  const userPortraitSubstitute = 'https://trello-attachments.s3.amazonaws.com/5d8c0d9e9b5de27371d1fc17/111x235/6d67ae9f110b59ad39ea8c38852d3a62/salt_shaker.png'
+  
+  const saveFavorite = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('/comments/newfav', {"comment":  id})
+    .then(res => console.log("Saved Res: ", res))
+    .catch(err => console.log(err))
+  }
 
 
-  <Comment.Group >
-    <Comment className="commentComponent">
-      <Comment.Avatar className="commentIMG" as='a' src={props.portrait} alt="salty user" />
-      <Comment.Content>
-        <Comment.Author className="usernameFontWeight"> {props.author}</Comment.Author>
-        <Comment.Text className="userScore">Score: {props.score.toFixed(2) * 100}</Comment.Text>
-        <Comment.Text >
-          “ {props.comment} ”
-          </Comment.Text>
-        <Comment.Actions>
-          {/* <Comment.Action>Reply</Comment.Action> */}
-          <Comment.Action className="commentSave">💾</Comment.Action>
-          <Comment.Action className="commentUpVote">👍</Comment.Action>
-          <Comment.Action className="commentDownVote">👎</Comment.Action>
-          {/* <Comment.Action>Hide</Comment.Action> */}
-          {/* <Comment.Action>
-              <Icon name='expand' />
-              Full-screen
-            </Comment.Action> */}
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-  </Comment.Group>
-
-)
+  return ( 
+    <>
+    <Comment.Group >
+      <Comment className="commentComponent">
+        <Comment.Avatar className="commentIMG" as='a' src={userPortraitSubstitute} alt="salty user" />
+        <Comment.Content>
+                                  {/*VV Using author instead of props.author  VV*/}
+          <Comment.Author className="usernameFontWeight"> {author}</Comment.Author>
+          <Comment.Text className="userScore">Score: {score.toFixed(3) * 1000 /* multiply by 1000 because a score of -1.672 isn't quite as cool as -1672 */}</Comment.Text>
+          <Comment.Text >
+            “ {comment} ”
+            </Comment.Text>
+          <Comment.Actions>
+            {/* <Comment.Action>Reply</Comment.Action> */}
+            <Comment.Action className="commentSave" onClick={saveFavorite}>💾</Comment.Action>
+            <Comment.Action className="commentUpVote">👍</Comment.Action>
+            <Comment.Action className="commentDownVote">👎</Comment.Action>
+            {/* <Comment.Action>Hide</Comment.Action> */}
+            {/* <Comment.Action>
+                <Icon name='expand' />
+                Full-screen
+              </Comment.Action> */}
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+    </Comment.Group>
+  </>
+  );
+};
 
 export default CommentCard;
 
