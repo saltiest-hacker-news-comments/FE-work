77ec76d3a7df62a4734c0420ab05324a5d98c3dd
Derek Etman <dereketman@gmail.com>
2020-06-26 13:27:36 -0700

    Refactor: cleaned up imports & fixed splash size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 // React Stuff
 import React, { useContext } from 'react';
-import { Route, Switch, Link } from "react-router-dom";
+import {  Link } from "react-router-dom";
 // Layout Stuf
-import { Container, Row, Col, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavLink, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
-import { Checkbox, Menu, Radio } from 'semantic-ui-react';
+import { Row, Col, Navbar, NavbarBrand } from "reactstrap";
+import {Radio} from "semantic-ui-react";
+
 // component Stuff
 import { useDarkMode } from '../hooks/useDarkMode';
-import { UserContext } from '../context/UserContext';
 // Nav Change 
 // import NavCondition from './Navs/NavCondition';
 import LoggedIn from './Navs/LoggedOut';
@@ -14,6 +14,10 @@ import LoggedOut from './Navs/LoggedIn';
 import NavCondition from './Navs/NavCondition';
 
 import { DarkModeSlider } from './DarkModeSlider/DarkModeSlider';
+
+
+export const RadioExampleToggle = () => <Radio toggle />
+
 
 const Header = () => {
     const [darkMode, setDarkMode] = useDarkMode(false);
@@ -30,7 +34,7 @@ const Header = () => {
                 <Col>
                     <Navbar className="header" expand="sm">
                         <NavbarBrand tag={Link} to="/">
-                            <img className="logo" src="https://trello-attachments.s3.amazonaws.com/5d89a54e06bb0a3a361741e9/975x347/87efc95a769db28dc466ae69284802fd/Salt_full_logo.png" />
+                            <img className="logo" alt="Saltines logo: A salt shaker tipped on its side." src="https://trello-attachments.s3.amazonaws.com/5d89a54e06bb0a3a361741e9/975x347/87efc95a769db28dc466ae69284802fd/Salt_full_logo.png" />
                         </NavbarBrand>
 
                         <NavCondition />
@@ -45,7 +49,7 @@ const Header = () => {
                             <p id="salt"> Salt </p>
 
                             <div className="ui fitted toggle checkbox">
-                                <input id="radio" type="radio" readOnly="" tabIndex="0" onClick={toggleDark} class="hidden" className={darkMode ? 'toggle toggled' : 'toggle'} />
+                                <input id="radio" type="Radio" readOnly="" tabIndex="0" onClick={toggleDark} class="hidden" className={darkMode ? 'toggle toggled' : 'toggle'} />
                                 <label />
                             </div>
 
--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -4,15 +4,34 @@ import { Divider } from "semantic-ui-react"
 import { TopUsers } from "./TopUsers"
 // import { TopCommentsCard } from "./TopCommentsCard"
 import { CommentList } from "./../CommentList"
+import {fabric} from 'fabric'
 
 const Landing = () => {
+
+    var canvas = new fabric.Canvas('c');
+
+    var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+    });
+
+    // "add" rectangle onto canvas
+    canvas.add(rect);
+
     return (
         <div className="landingPage">
+
+            {/* <canvas id='c' width='500' height='500' style={{border:'1px solid black'}}></canvas> */}
+            
             <Jumbotron className="landingHeader" style={{ background: '#f97e03', margin: '0' }}>
-                <img className="splash" src="https://trello-attachments.s3.amazonaws.com/5d8d4f4e8545a25e7b3e1dfd/932x511/3192b9f59741391d23d2fc1e1896def3/Full_Saltines_Splash.png" />
-                {/* <h1 className="jumbo-head">Saltines</h1>
-                <h3 className="subhead">A Hacker News Troll Ranker</h3> */}
+                <img className="splash" alt="Salt is being poured onto a saltine cracker with the, Behind the text displays - Saltines: A Hacker News Troll Ranker" src="https://trello-attachments.s3.amazonaws.com/5d8d4f4e8545a25e7b3e1dfd/932x511/3192b9f59741391d23d2fc1e1896def3/Full_Saltines_Splash.png" />
+                {/* <h1 className="jumbo-head">Saltines</h1> */}
+                {/* <h3 className="subhead">A Hacker News Troll Ranker</h3> */}
             </Jumbotron>
+
             <Divider section className="section-divider" />
             <TopUsers />
             <Divider section className="section-divider" />
